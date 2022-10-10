1e13f4fd08c254654371923abebf86d7cb142f1b
Ольга <koval.oi.1106@gmail.com>
2022-10-10 22:26:03 +0500

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,28 @@ public class Main {
         human.name = "Максим";
         human.yearOfBirth = 1987;
         human.town= "Минск";
+        human.job = "бренд-менеджер";
         human.acquaintance ();
 
         human anna = new human();
         human.name = "Анна";
         human.yearOfBirth = 1993;
         human.town = "Москва";
+        human.job = "методист образовательных программ";
         human.acquaintance();
 
         human katya = new human();
         human.name = "Катя";
         human.yearOfBirth = 1994;
         human.town = "Калиниград";
+        human.job = "продакт-менеджер";
         human.acquaintance();
 
         human artem = new human();
         human.name = "Артем";
         human.yearOfBirth = 1995;
         human.town = "Москва";
+        human.job = "директор по развитию бизнеса";
         human.acquaintance();
 
 
--- src/human.java
+++ src/human.java
@@ -2,9 +2,10 @@ public class human {
     public static String name;
     public static String town;
     public static int yearOfBirth;
+    public static String job;
 
     static void acquaintance (){
-        System.out.println("Привет! Меня зовут"+ " " + name+"."+"Я из города"+ " " + town +"." +"Я родился в"+ " " + yearOfBirth + ".");
+        System.out.println("Привет! Меня зовут"+ " " + name+"."+"Я из города"+ " " + town +"." +"Я родился в"+ " " + yearOfBirth + ". Я работаю на должности" + " " + job+ ". Будем знакомы!");
     }
 
 }
